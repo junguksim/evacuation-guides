56b50ef4d127e87bb53d817fe2714267dbedda00
junguksim <simju9397@gmail.com>
2021-11-08 22:54:51 +0900

    Add homepage link

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={ process.env.PUBLIC_URL}>
     <Provider store={store}>
       <React.StrictMode>
         <App/>
